5d09b1495c838a0aa5f31690a7b3543f6ca1bced
TonyPFrancis <tony.612.4u@gmail.com>
2015-07-22 12:09:14 +0530

    updating items file wrt req change

--- spiceworks/spiceworks/items.py
+++ spiceworks/spiceworks/items.py
@@ -10,37 +10,25 @@ from scrapy.item import Field, Item
 
 class MainItem(Item):
     # define the fields for your Main item here like:
+    product_title = Field()
     model_number = Field()
-    number_of_replies = Field()
     product_rating = Field()
     description = Field()
-    reviews = Field()
-    mentions = Field()
+    total_number_of_reviews = Field()
     review_by = Field()
     review_at = Field()
     review = Field()
     review_rating = Field()
-    mention_link_fk = Field()
-    mention_by = Field()
-    resource = Field()
 
 class TopicItem(Item):
     # define the fields for your Topic item here like:
+    product_title = Field()
+    total_number_of_mentions = Field()
     mention_link_fk = Field()
-    topic_by = Field()
-    timestamp = Field()
-    title = Field()
-    topic = Field()
-    resource = Field()
+    mention_title = Field()
+    mention_content = Field()
+    mention_by = Field()
+    total_number_of_replies = Field()
     reply_by = Field()
     reply_at = Field()
     reply = Field()
-    number_of_replies = Field()
-
-
-
-
-
-
-
-
